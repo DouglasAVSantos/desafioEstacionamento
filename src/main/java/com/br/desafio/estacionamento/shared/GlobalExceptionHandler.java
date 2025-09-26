08b35fc64b9bac18a5d39342a6d9a9376e4bfece
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-09-26 19:45:44 -0300

    feat: cria GlobalExceptionHandler para centralizar o tratamento de exceções.

---
+++ src/main/java/com/br/desafio/estacionamento/shared/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.br.desafio.estacionamento.shared;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<Map<String,Object>>handleConflict(ConflictException ex){
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("horario", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss - dd/MM/yyyy")));
+        map.put("erro",ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(map);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String,Object>>handleNotFound(NotFoundException ex){
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("horario", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss - dd/MM/yyyy")));
+        map.put("erro",ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(map);
+    }
+}
