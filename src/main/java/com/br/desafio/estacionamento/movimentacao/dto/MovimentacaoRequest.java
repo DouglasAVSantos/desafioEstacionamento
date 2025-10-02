bea791f14f034d64e38869b08a41931f287c7bac
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-10-02 18:51:40 -0300

    feat: ajustado para conter só o response, a criação da entidade vai ficar no MovimentacaoService

--- src/main/java/com/br/desafio/estacionamento/movimentacao/dto/MovimentacaoRequest.java
+++ src/main/java/com/br/desafio/estacionamento/movimentacao/dto/MovimentacaoRequest.java
@@ -1,9 +1,6 @@
 package com.br.desafio.estacionamento.movimentacao.dto;
 
-import com.br.desafio.estacionamento.vaga.entity.Vaga;
-import com.br.desafio.estacionamento.veiculo.entity.Veiculo;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record MovimentacaoRequest(
         @NotBlank(message = "Vaga é obrigatório")
