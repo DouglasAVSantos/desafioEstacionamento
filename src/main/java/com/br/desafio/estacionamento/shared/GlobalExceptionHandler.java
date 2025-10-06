dbfe77209439484b6e890f66d57e0ec9c38a70a6
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-10-06 19:28:12 -0300

    feat: criado o metodo que valida ENUMS

--- src/main/java/com/br/desafio/estacionamento/shared/GlobalExceptionHandler.java
+++ src/main/java/com/br/desafio/estacionamento/shared/GlobalExceptionHandler.java
@@ -36,4 +36,18 @@ public class GlobalExceptionHandler {
         map.put("erro",ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
     }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String,Object>>handle(MethodArgumentTypeMismatchException ex){
+        if (ex.getRequiredType().isEnum()) {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "erro", "Valor inválido para o parâmetro " + ex.getName() +
+                            ". Valores aceitos: " + Arrays.toString(ex.getRequiredType().getEnumConstants())
+            ));
+        }
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("horario", LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss - dd/MM/yyyy")));
+        map.put("erro",ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(map);
+    }
 }
